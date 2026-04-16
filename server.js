d8622ed096d43c64bf2f1378e87b3713971c7e19
Ratnesh Kushwaha <89016010+RatKush@users.noreply.github.com>
2026-04-16 19:44:35 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -137,6 +137,6 @@ app.get('/data', (req, res) => {
   res.json({ sheets: STORED_DATA });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
